2dc7d11cc5bedc1bb9b227f22f3a9f4a6df48e28
BHANU PRATAP SINGH <70773520+bhanu9672@users.noreply.github.com>
2025-11-30 01:32:13 +0530

    New Code For vercel

--- server/server.js
+++ server/server.js
@@ -12,37 +12,37 @@ const app = express()
 const server = http.createServer(app)
 
 // Initialize Socket.io Server
-export const io = new Server( server, {
-    cors: { origin : "*" }
-} )
+export const io = new Server(server, {
+    cors: { origin: "*" }
+})
 
 // Store Online Users
 export const userSocketMap = {} // { userId: socketId }
 
 // Socket.on Connection Handler
-io.on( "connection", (socket) => {
+io.on("connection", (socket) => {
     const userId = socket.handshake.query.userId;
-    console.log( "User Connected", userId )
+    console.log("User Connected", userId)
 
-    if( userId ) {
+    if (userId) {
         userSocketMap[userId] = socket.id;
-    } 
+    }
 
     // Emit Online Users to all Connecteed Clients
-    io.emit( "getOnlineUsers", Object.keys(userSocketMap) )
+    io.emit("getOnlineUsers", Object.keys(userSocketMap))
 
-    socket.on( "disconnect", () => {
-        console.log( "User Disconnected", userId )
+    socket.on("disconnect", () => {
+        console.log("User Disconnected", userId)
         delete userSocketMap[userId]
         io.emit("getOnlineUsers", Object.keys(userSocketMap))
-    } )
+    })
 })
 
 // Setup Middleware 
-app.use(express.json({ limit: "4mb" })) 
+app.use(express.json({ limit: "4mb" }))
 app.use(cors())
-app.use(express.json()); 
-app.use(express.urlencoded({ extended: true })); 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // Routes Setup 
 app.use("/api/status", (req, res) => res.send("Server is Live"))
@@ -52,7 +52,12 @@ app.use("/api/messages", messageRouter)
 // Connected With MongoDb Database
 await connectDB()
 
-const PORT = process.env.PORT || 5000;
-server.listen(PORT, () => {
-    console.log(`Server is Running on Port: ${PORT}`)
-})
+if (process.env.NODE_ENV !== "production") {
+    const PORT = process.env.PORT || 5000;
+    server.listen(PORT, () => {
+        console.log(`Server is Running on Port: ${PORT}`)
+    })
+}
+
+// Export Server For vercel
+export default server
